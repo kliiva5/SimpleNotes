72a44cdc81593702724fde01147a5e2591a2d312
Kristjan Liiva <kliiva5@users.noreply.github.com>
2018-01-25 18:05:31 +0200

    Feature/#1 - Homepage starting design (#8)
    * Added starting methods and functions for Homepage
    
    * Added simple homepage layout for now

--- app/src/main/java/com/example/kristjan/simplenotes/MainActivity.java
+++ app/src/main/java/com/example/kristjan/simplenotes/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.kristjan.simplenotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize authentication
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if the user is already singed in update the UI accordingly
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    /**
+     * @param user - describes the currently signed in user
+     *
+     *  Method for updating the UI according to sign-in status
+     */
+    private void updateUI(FirebaseUser user) {
+        if(user != null) {
+            // TODO: Get the current user's profile data and set it to the top bar
+        } else {
+            // The user was not signed in show the default homepage (MainActivity in this case)
+            // TODO: Hide the required elements when the user is not signed in
+        }
+    }
+
+    /* Called when the user taps the Begin button */
+    public void beginButton(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        // Finish the current activity to prevent back button loops
+        finish();
+        startActivity(intent);
+    }
+
 }
